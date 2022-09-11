0dc848f96b30e7f4198d47c607406c460818a4e7
Your Name <lucasdosualdo@gmail.com>
2022-09-11 18:38:29 -0300

    docs: validate signup fields

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import cors from 'cors';
-import {MongoClient} from 'mongodb';
-import dotenv from 'dotenv';
-import joi from 'joi';
-import bcrypt from 'bcrypt';
-import {v4 as uuid} from 'uuid';
+import express from "express";
+import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import joi from "joi";
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 const app = express();
 app.use(express.json());
@@ -12,9 +12,58 @@ app.use(cors());
 dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect().then(() => {
+  db = mongoClient.db("project13-mywallet");
+});
 
+const signUpSchema = joi.object({
+  name: joi.string().required().trim(),
+  email: joi.string().required().email(),
+  password: joi.string().required().trim(),
+});
 
+app.post("/signup", async (req, res) => {
+  const { name, email, password } = req.body;
+  const validation = signUpSchema.validate(req.body, { abortEarly: false });
+  if (validation.error) {
+    const err = validation.error.details.map((err) => err.message);
+    res.status(422).send(err);
+    return;
+  }
 
+  const passwordHash = bcrypt.hashSync(password, 10);
 
+  const validatePassword = bcrypt.compareSync("nn22", passwordHash);
+  console.log(passwordHash, validatePassword);
 
-app.listen(5000);
+  try {
+    //verificando se já existe email/senha
+    const existingEmail = await db.collection("signup").findOne({ email });
+    if (existingEmail) {
+      return res.status(409).send("Email já existente");
+    }
+    const passwordList = await db.collection("signup").find({}).toArray();
+    const existingPassword = passwordList.filter((value) =>
+      bcrypt.compareSync(password, value.password)
+    );
+
+    console.log(passwordList, existingPassword);
+    if(existingPassword.length !== 0){
+        return res.status(409).send('Senha já existente');
+    }
+    //adicionando ao db
+    const user = await db.collection("signup").insertOne({
+      name,
+      email,
+      password: passwordHash,
+    });
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.listen(5000, () => {
+  console.log("listening on 5000 port");
+});
